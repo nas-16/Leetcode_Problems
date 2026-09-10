84e0cdc48c84a9457efe38ea1bb332745d5dafaf
nas-16 <nasuruddinshaikbadar@gmail.com>
2026-09-10 22:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (15.02%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i=0
+        for j in range(len(nums)):
+            if nums[j]!=nums[i]:
+                nums[i+1]=nums[j]
+                i=i+1
+        return i+1
+        
+ 
+
+        
